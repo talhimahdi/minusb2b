921bbfb5726e7243560ce29a03e2ef64392df5be
talhimahdi <talhi.mahd@gmail.com>
2022-09-11 01:44:36 +0100

    check page name

--- components/Layout.js
+++ components/Layout.js
@@ -7,22 +7,29 @@ import Footer from "./Footer";
 export default function Layout({ children }) {
   console.log(children?.type);
   console.log(children);
-  return (
-    <div>
-      <Head>
-        <title>Minus B2B</title>
-        <meta content="upgrade-insecure-requests" />
-        <meta name="description" content="Minus B2B" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main>
-        <TopNavbar pageName={children?.type?.name} />
-        <Header pageName={children?.type?.name} />
 
-        {children?.type?.name}
-        {children}
-      </main>
-      <Footer withMargin={children?.type?.name == "Products" ? true : false} />
-    </div>
-  );
+  if (children?.type?.name != "o") {
+    return (
+      <div>
+        <Head>
+          <title>Minus B2B</title>
+          <meta content="upgrade-insecure-requests" />
+          <meta name="description" content="Minus B2B" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <main>
+          <TopNavbar pageName={children?.type?.name} />
+          <Header pageName={children?.type?.name} />
+
+          {children?.type?.name}
+          {children}
+        </main>
+        <Footer
+          withMargin={children?.type?.name == "Products" ? true : false}
+        />
+      </div>
+    );
+  } else {
+    return <></>;
+  }
 }
