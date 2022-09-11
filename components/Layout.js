91ca0944e09271cb98ce1f7a26bf79bdd6d0a6eb
talhimahdi <talhi.mahd@gmail.com>
2022-09-11 01:20:43 +0100

    fix header

--- components/Layout.js
+++ components/Layout.js
@@ -14,11 +14,11 @@ export default function Layout({ children }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <TopNavbar pageName={children.type.name} />
-        <Header pageName={children.type.name} />
+        <TopNavbar pageName={children?.type?.name} />
+        <Header pageName={children?.type?.name} />
         {children}
       </main>
-      <Footer withMargin={children.type.name == "Products" ? true : false} />
+      <Footer withMargin={children?.type?.name == "Products" ? true : false} />
     </div>
   );
 }
